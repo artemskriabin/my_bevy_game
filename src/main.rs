0440a43b2d6c457d8c43b389d4d480b88fbda79b
Skriabin <artem.skriabin@distributedlab.com>
2023-05-23 13:25:22 +0300

    Add floor and camera, add camera rotation control

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use bevy::app::{App, Plugin};
+use bevy::asset::Assets;
+use bevy::input::Input;
+use bevy::math::{EulerRot, Quat, Vec3};
+use bevy::pbr::{PbrBundle, StandardMaterial};
+use bevy::prelude::{
+    shape, Camera, Camera3d, Camera3dBundle, Color, Commands, GlobalTransform, KeyCode, Mesh,
+    Query, Res, ResMut, Transform, With,
+};
+use bevy::time::Time;
+use bevy::utils::default;
 use bevy::DefaultPlugins;
-use bevy::prelude::{Commands, Component, Query, Res, ResMut, Resource, With};
-use bevy::time::{Time, Timer, TimerMode};
 
 fn main() {
     App::new()
@@ -14,43 +22,44 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+        app.add_startup_system(setup_env).add_system(control_camera);
     }
 }
 
-
-
-
-fn hello_world() {
-    println!("hello worlde!");
+fn setup_env(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
-#[derive(Component)]
-struct Person;
+fn control_camera(
+    time: Res<Time>,
+    mut camera: Query<(&mut Camera, &mut Transform, &GlobalTransform), With<Camera3d>>,
+    input: Res<Input<KeyCode>>,
+) {
+    let (_, mut camera_transform, camera_global_transform) = camera.single_mut();
 
+    let rotation = if input.pressed(KeyCode::Left) {
+        time.delta_seconds()
+    } else if input.pressed(KeyCode::Right) {
+        -time.delta_seconds()
+    } else {
+        0.0
+    };
 
-#[derive(Component)]
-struct Name(String);
-
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Name("Zayna Nieves".to_string()), Person));
+    camera_transform.rotate_around(
+        camera_global_transform.translation(),
+        Quat::from_euler(EulerRot::XYZ, 0.0, rotation, 0.0),
+    );
 }
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-
-
